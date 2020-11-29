c38a073b26c17f5d87216f3123934dcd77d09590
BlinkovMark <markblinkov1@gmail.com>
2020-11-29 20:35:07 +0200

    4.3.6. Math.abs [#400719]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -9,5 +9,4 @@ public class ChessBoard {
         }
         return rsl;
     }
-
 }
--- test/ru/job4j/condition/ChessBoardTest.java
+++ test/ru/job4j/condition/ChessBoardTest.java
@@ -23,5 +23,4 @@ public class ChessBoardTest {
         int way = ChessBoard.way(0, 0, 5, 6);
         assertThat(way, is(0));
     }
-
 }
