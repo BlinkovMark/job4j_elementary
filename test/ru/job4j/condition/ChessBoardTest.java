b800279c0f4b6b63d0a9937d77d9dfb2bfbaff1f
BlinkovMark <markblinkov1@gmail.com>
2020-11-30 00:15:56 +0200

    4.3.6. Math.abs [#400719]

--- test/ru/job4j/condition/ChessBoardTest.java
+++ test/ru/job4j/condition/ChessBoardTest.java
@@ -8,19 +8,19 @@ import static org.junit.Assert.*;
 public class ChessBoardTest {
     @Test
     public void wayIs6() {
-        int way = ChessBoard.way(0, 0, 0, 6);
-        assertThat(way, is(6));
+         int way = ChessBoard.way(0, 0, 0, 6);
+         assertThat(way, is(6));
     }
 
     @Test
     public void wayIs5() {
-        int way = ChessBoard.way(0, 0, 5, 0);
-        assertThat(way, is(5));
+         int way = ChessBoard.way(0, 0, 5, 0);
+         assertThat(way, is(5));
     }
 
     @Test
     public void wayIs0() {
-        int way = ChessBoard.way(0, 0, 5, 6);
-        assertThat(way, is(0));
+         int way = ChessBoard.way(0, 0, 5, 6);
+         assertThat(way, is(0));
     }
 }
