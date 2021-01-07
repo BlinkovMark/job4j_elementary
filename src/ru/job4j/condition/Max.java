3cad2e2f4d8a4fabd92fd9bbe4105f5a0c4a0e07
BlinkovMark <markblinkov1@gmail.com>
2021-01-08 01:29:14 +0200

    3. Перегрузить метод max для трех чисел [#400919]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,14 +1,21 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        boolean condition = left >= right;
-        int result = condition ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        boolean a = first >= second;
+        return a ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+        public static int max(int first, int second, int third, int fourth) {
+            return max(first, second, max(third, fourth));
     }
 
     public static void main(String[] args) {
-        int result = Max.max(4, 5);
+        int result = Max.max(12, 15, 13, 14);
         System.out.println(result);
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -7,27 +7,33 @@ import static org.junit.Assert.assertThat;
     public class MaxTest {
 
         @Test
-        public void whenMax1To2Then2() {
-            int result = Max.max(1, 2);
-            assertThat(result, is(2));
+        public void whenMax4() {
+            int result = Max.max(1, 2, 4, 3);
+            assertThat(result, is(4));
         }
 
         @Test
-        public void whenMax10To1Then10() {
-            int result = Max.max(10, 1);
+        public void whenMax10() {
+            int result = Max.max(10, 6, 1, 8);
             assertThat(result, is(10));
         }
 
         @Test
-        public void whenMax5To20Then20() {
-            int result = Max.max(5, 20);
-            assertThat(result, is(20));
+        public void whenMax24() {
+            int result = Max.max(5, 24, 8, 13);
+            assertThat(result, is(24));
         }
 
         @Test
-        public void whenMax15To15Then15() {
-            int result = Max.max(15, 15);
+        public void whenMax15() {
+            int result = Max.max(12, 9, 15, 8);
             assertThat(result, is(15));
+        }
+
+        @Test
+        public void whenAllEquals() {
+            int result = Max.max(100, 100, 100, 100);
+            assertThat(result, is(100));
         }
     }
 
